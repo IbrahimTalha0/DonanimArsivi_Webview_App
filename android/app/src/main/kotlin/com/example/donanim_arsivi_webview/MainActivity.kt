75b2e822a5a567a1c8fe319da247a897826036cd
İbrahim Talha DEMİR <75858218+IbrahimTalha0@users.noreply.github.com>
2021-06-13 06:22:52 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/donanim_arsivi_webview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.donanim_arsivi_webview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Donanım Arşivi Forum',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({ Key? key }) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: 'https://forum.donanimarsivi.com/',
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
